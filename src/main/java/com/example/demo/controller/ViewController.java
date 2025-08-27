d013eefde806d6f7ccf49d6d62810c6c7ff2921a
Hyeoun_Ju <wjdguswn0954@gmail.com>
2025-08-27 19:53:15 +0900

    페이지 디자인 수정

--- src/main/java/com/example/demo/controller/ViewController.java
+++ src/main/java/com/example/demo/controller/ViewController.java
@@ -1,12 +1,13 @@
 package com.example.demo.controller;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
     @GetMapping("/")
-    public String home() {
+    public String index(Model model) {
         return "index";
     }
 
